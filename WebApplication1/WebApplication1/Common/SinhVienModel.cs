ed3bd2d6ee6fa4aada582b64fc4bc456b643cb4b
hoangbuutran <33544451+hoangbuutran@users.noreply.github.com>
2018-02-03 19:51:49 +0700

    7h51-3/2/18

---
+++ WebApplication1/WebApplication1/Common/SinhVienModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Common
+{
+    public class SinhVienModel
+    {
+        public int IdSinhVien { get; set; }
+        public string TenDangNhap { get; set; }
+        public string MatKhau { get; set; }
+        public bool NhoMatKhau { get; set; }
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Common;
+
+namespace WebApplication1.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(SinhVienModel sinhvien)
+        {
+
+            return View();
+        }
+    }
+}
