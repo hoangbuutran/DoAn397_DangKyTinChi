ebd46dd4817857951dafa28c9536c3c311971492
hoangbuutran <33544451+hoangbuutran@users.noreply.github.com>
2018-02-28 10:51:54 +0700

    10h51-28/2/2018

--- WebApplication1/WebApplication1/Areas/GiaoVu/Models/KetQuaThongKe.cs
+++ WebApplication1/WebApplication1/Areas/GiaoVu/Models/KetQuaThongKe.cs
@@ -7,6 +7,7 @@ namespace WebApplication1.Areas.GiaoVu.Models
 {
     public class KetQuaThongKe
     {
+        public string MA_MON_HOC { get; set; }
         public string TEN_MON_HOC { get; set; }
         public int TONGCHIMON { get; set; }
     }
