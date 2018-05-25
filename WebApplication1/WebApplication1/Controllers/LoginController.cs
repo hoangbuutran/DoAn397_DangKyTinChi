dfb4390aa89b024f35cfc1328c283e5a63d93883
HoangBuuTran <tranhoangbuu66@gmail.com>
2018-05-25 09:43:09 +0700

    push lan cuoi

--- WebApplication1/WebApplication1/Controllers/LoginController.cs
+++ WebApplication1/WebApplication1/Controllers/LoginController.cs
@@ -1,10 +1,7 @@
 ﻿using Model.DAO;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using WebApplication1.Common;
+using CaptchaMvc.HtmlHelpers;
 
 namespace WebApplication1.Controllers
 {
@@ -18,59 +15,67 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public ActionResult Login(SinhVienModel sinhvien)
         {
-            var daoTaiKhoan = new TaiKhoanDao();
-            var i = daoTaiKhoan.Login(sinhvien.TenDangNhap, sinhvien.MatKhau);
-            if (i == 1)//ad
+            if (this.IsCaptchaValid("Captcha is not valid"))
             {
-                var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
-                var daoAdmin = new NhanVienDao().NhanVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
-                var sinhviensession = new SinhVienModel
+                var daoTaiKhoan = new TaiKhoanDao();
+                var i = daoTaiKhoan.Login(sinhvien.TenDangNhap, sinhvien.MatKhau);
+                if (i == 1)//ad
                 {
-                    IdSinhVien = daoAdmin.ID_NHANVIEN,
-                    TenDangNhap = dao.USERNAME,
-                    MatKhau = dao.PASS,
-                    idloainguoidung = dao.ID_QUYEN
-                };
-                Session.Add("USER_SESSION", sinhviensession);
-                return RedirectToAction("Index", "Home", new { area = "Admin" });
-            }
-            else if (i == 3)//sunh vien
-            {
-                var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
-                var daosinhvien = new SinhVienDao().SinhVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
-                var sinhviensession = new SinhVienModel
+                    var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
+                    var daoAdmin = new NhanVienDao().NhanVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
+                    var sinhviensession = new SinhVienModel
+                    {
+                        IdSinhVien = daoAdmin.ID_NHANVIEN,
+                        TenDangNhap = dao.USERNAME,
+                        MatKhau = dao.PASS,
+                        idloainguoidung = dao.ID_QUYEN
+                    };
+                    Session.Add("USER_SESSION", sinhviensession);
+                    return RedirectToAction("Index", "Home", new { area = "Admin" });
+                }
+                else if (i == 3)//sunh vien
                 {
-                    IdSinhVien = daosinhvien.ID_SINHVIEN,
-                    TenDangNhap = daosinhvien.TAIKHOAN.USERNAME,
-                    MatKhau = daosinhvien.TAIKHOAN.PASS,
-                    MaSinhVien = daosinhvien.MA_SINH_VIEN,
-                    idChuyenNganh = daosinhvien.ID_CHUYEN_NGANH
-                };
-                Session.Add("USER_SESSION", sinhviensession);
-                return RedirectToAction("Index", "Home");//sinh vien
-            }
-            else if (i == 2)// giao vu
-            {
-                var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
-                var daoGiaoVu = new NhanVienDao().NhanVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
-                var sinhviensession = new SinhVienModel
+                    var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
+                    var daosinhvien = new SinhVienDao().SinhVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
+                    var sinhviensession = new SinhVienModel
+                    {
+                        IdSinhVien = daosinhvien.ID_SINHVIEN,
+                        TenDangNhap = daosinhvien.TAIKHOAN.USERNAME,
+                        MatKhau = daosinhvien.TAIKHOAN.PASS,
+                        MaSinhVien = daosinhvien.MA_SINH_VIEN,
+                        idChuyenNganh = daosinhvien.ID_CHUYEN_NGANH
+                    };
+                    Session.Add("USER_SESSION", sinhviensession);
+                    return RedirectToAction("Index", "Home");//sinh vien
+                }
+                else if (i == 2)// giao vu
                 {
-                    IdSinhVien = daoGiaoVu.ID_NHANVIEN,
-                    TenDangNhap = dao.USERNAME,
-                    MatKhau = dao.PASS,
-                    idloainguoidung = dao.ID_QUYEN
-                };
-                Session.Add("USER_SESSION", sinhviensession);
-                return RedirectToAction("Index", "Home", new { area = "GiaoVu" });
-            }
-            else if (i == 0)
-            {
-                ModelState.AddModelError("", "Tài khoản thuộc quyền bị khóa");
-                return View();
+                    var dao = daoTaiKhoan.TaiKhoanSingle(sinhvien.TenDangNhap, sinhvien.MatKhau);
+                    var daoGiaoVu = new NhanVienDao().NhanVienSingerWithIDTaiKhoan(dao.ID_TAI_KHOAN);
+                    var sinhviensession = new SinhVienModel
+                    {
+                        IdSinhVien = daoGiaoVu.ID_NHANVIEN,
+                        TenDangNhap = dao.USERNAME,
+                        MatKhau = dao.PASS,
+                        idloainguoidung = dao.ID_QUYEN
+                    };
+                    Session.Add("USER_SESSION", sinhviensession);
+                    return RedirectToAction("Index", "Home", new { area = "GiaoVu" });
+                }
+                else if (i == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không đúng");
+                    return View();
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Tài Khoản Không Đúng");
+                    return View();
+                }
             }
             else
             {
-                ModelState.AddModelError("", "Tài Khoản Không Đúng");
+                ModelState.AddModelError("", "Mã CAPTCHA không đúng");
                 return View();
             }
         }
